54404289ea6b79c87d0a547ec1603956d922b6fb
KillerDogeEmpire <87155550+KillerDogeEmpire@users.noreply.github.com>
2022-06-16 17:41:56 -0700

    Update KrunchyProvider.kt

--- app/src/main/java/com/lagradost/cloudstream3/animeproviders/KrunchyProvider.kt
+++ app/src/main/java/com/lagradost/cloudstream3/animeproviders/KrunchyProvider.kt
@@ -75,7 +75,7 @@ class KrunchyProvider : MainAPI() {
 
     override var mainUrl = "http://www.crunchyroll.com"
     override var name: String = "Crunchyroll"
-    override val lang = "en"
+    override var lang = "en"
     override val hasQuickSearch: Boolean
         get() = false
     override val hasMainPage: Boolean
